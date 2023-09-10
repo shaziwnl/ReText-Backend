9f395d360fd8f88fcca7fa9dd026635518281aad
shaziwnl <121330440+shaziwnl@users.noreply.github.com>
2023-09-10 08:45:18 -0400

    Update openaiapi.py

--- openaiapi.py
+++ openaiapi.py
@@ -9,7 +9,6 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def chat_completion(task, content):
   completion = openai.ChatCompletion.create(
     temperature=0.2,
-    max_tokens=100,
     model="gpt-3.5-turbo",
     messages=[
       # {"role": "system", "content": "You are a writing assistant that helps the user with correcting grammatical mistakes in their text, or restructure sentences to make the meaning of the texts more clear or concise."},
