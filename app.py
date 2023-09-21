339a6b73b528fff570378e78cac8105703b690d3
Pranav Vyas <56647593+bobthesnek63@users.noreply.github.com>
2023-09-21 16:27:26 -0400

    Update prompts
    Updated the prompts for the features.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def verbose():
     if request.method == 'POST':
         sentence = request.get_json().get('sentence')
         # return jsonify({"sentence": sentence})
-        return ( {"completion" : chat_completion("clearer to understand", sentence)} )
+        return ( {"completion" : chat_completion("You are a writing assistant that adds detail to texts. Add details to the following text", sentence)} )
 
 
 @app.route('/concise', methods=['POST'])
@@ -23,7 +23,7 @@ def concise():
     if request.method == 'POST':
         sentence = request.get_json().get('sentence')
         # return jsonify({"sentence": sentence})
-        return ( {"completion" : chat_completion("less wordy and easier to read", sentence)} )
+        return ( {"completion" : chat_completion("You are a writing assistant that helps make texts more concise. Make the following text more concise", sentence)} )
     
 
 @app.route('/rectify', methods=['POST'])
@@ -31,7 +31,7 @@ def rectify():
     if request.method == 'POST':
         sentence = request.get_json().get('sentence')
         # return jsonify({"sentence": sentence})
-        return ( {"completion" : chat_completion("grammatically correct", sentence)} )
+        return ( {"completion" : chat_completion("You are a writing assistant that helps the user with correcting grammatical mistakes in their text. Correct the mistakes in the following text", sentence)} )
     
     
 @app.route('/formalize', methods=['POST'])
@@ -39,7 +39,7 @@ def formalize():
     if request.method == 'POST':
         sentence = request.get_json().get('sentence')
         # return jsonify({"sentence": sentence})
-        return ( {"completion" : chat_completion("formalize", sentence)} )
+        return ( {"completion" : chat_completion("I need to send a message to my boss. Please make the following message more formal", sentence)} )
 
 
 if __name__ == "__main__":
